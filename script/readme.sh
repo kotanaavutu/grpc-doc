a3a9d30a70ba9a77d3efbe1aae098310c2558ea4
kotana Avatharam <kotanaavutu@gmail.com>
2022-07-08 23:23:15 +0530

    update action file

--- script/readme.sh
+++ script/readme.sh
@@ -5,12 +5,12 @@ current_ip="$(curl --silent --show-error --fail ipecho.net/plain)"
 echo "IP: $current_ip"
 
 # Update A record
-curl -X multi "https://dash.readme.com/api/v1/docs" \
+curl -X POST "https://dash.readme.com/api/v1/docs" \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
     -H "Authorization: Basic cmRtZV94bjhzOWgwYWY3YTIwMGU3ZWY3YWIxZGJjMjBmYTkyMmI5ZDBmZGQ1NDAyMGEyYWE3OTQ3NGQ3MTk2ODliOGYyYmY4NjgwOg==" \
     -H "Accept: application/json" \
-    --data @- <<END;
+    --data-binary @- <<END;
 { 
     "hidden": false, 
     "order": 999, 
